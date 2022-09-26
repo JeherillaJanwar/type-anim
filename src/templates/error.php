4d9ec6d98b6402dd893ef2b67fb2beffeb600565
Ishaan Sharma <ishaan328069@gmail.com>
2022-09-26 08:53:08 -0400

    Create error.php

---
+++ src/templates/error.php
@@ -0,0 +1,11 @@
+
+<svg xmlns='http://www.w3.org/2000/svg'
+    xmlns:xlink='http://www.w3.org/1999/xlink'
+    viewBox='0 0 400 50'
+    width='400px' height='50px'>
+
+    <text font-family='monospace' fill='#c00' font-size='18'
+        y="50%" x='50%' dominant-baseline='middle' text-anchor='middle'>
+        <?= $message . "\n" ?>
+    </text>
+</svg>
