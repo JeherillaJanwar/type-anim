9de391a9b2a703340ffb83068fef82108c1ba6fd
Ishaan Sharma <ishaan328069@gmail.com>
2022-12-24 14:19:44 +0000

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+require "../vendor/autoload.php";
+
+// load environment variables if .env exists
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+$controller = new RendererController($_REQUEST);
+$controller->setHeaders();
+echo $controller->render();
