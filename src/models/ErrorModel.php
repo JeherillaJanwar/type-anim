30b38daa7751d3e4cbe8d9d772b10fc861c216ad
Ishaan Sharma <ishaan328069@gmail.com>
2022-12-24 09:21:36 -0500

    Create ErrorModel.php

---
+++ src/models/ErrorModel.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+/**
+ * Model for error messages
+ */
+class ErrorModel
+{
+    /** @var string $message Text to display */
+    public $message;
+
+    /** @var string $template Path to template file */
+    public $template;
+
+    /**
+     * Construct ErrorModel
+     *
+     * @param string $message Text to display
+     * @param string $template Path to the template file
+     */
+    public function __construct($template, $message)
+    {
+        $this->message = $message;
+        $this->template = $template;
+    }
+}
