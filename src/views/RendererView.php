093f63b4b8e8b6b314a8c64fcb70979bd21273b5
Ishaan Sharma <ishaan328069@gmail.com>
2022-12-24 09:22:17 -0500

    Create RendererView.php

---
+++ src/views/RendererView.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+/**
+ * View for rendering typing SVG
+ */
+class RendererView
+{
+    /**
+     * @var RendererModel $model
+     */
+    private $model;
+
+    /**
+     * Constructor for Renderer View
+     * @param RendererModel $model
+     */
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Render SVG Output
+     * @return string
+     */
+    public function render()
+    {
+        // import variables into symbol table
+        extract([
+            "lines" => $this->model->lines,
+            "font" => $this->model->font,
+            "color" => $this->model->color,
+            "background" => $this->model->background,
+            "size" => $this->model->size,
+            "center" => $this->model->center,
+            "vCenter" => $this->model->vCenter,
+            "width" => $this->model->width,
+            "height" => $this->model->height,
+            "multiline" => $this->model->multiline,
+            "fontCSS" => $this->model->fontCSS,
+            "duration" => $this->model->duration,
+            "pause" => $this->model->pause,
+            "repeat" => $this->model->repeat,
+        ]);
+        // render SVG with output buffering
+        ob_start();
+        include $this->model->template;
+        $output = ob_get_contents();
+        ob_end_clean();
+        // return rendered output
+        return $output;
+    }
+}
